0da1ba3603272c2a2c7f71afd697ca213cd9614e
Polushin Dima <dima2004polushin2004@gmail.com>
2023-11-02 17:37:47 +0600

    Добавил проверку для api и очистил проект

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosApi = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'https://dpg.gg/test/calendar.json',
 });
 
 export default axiosApi;
